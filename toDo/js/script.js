fae49d5b3eb738210f0613dc1fc233896762141a
purplyrocky <purplyrocky@gmail.com>
2015-05-07 20:56:15 -0700

    delete button works meow

--- toDo/js/script.js
+++ toDo/js/script.js
@@ -20,32 +20,57 @@ var inputTask = document.getElementById('inputTask');
 function createTask(e){
     //prevents page from refreshing each time the button is clicked
     e.preventDefault();
-    //create array to store tasks
-    var tasks = [];
+    ////create array to store tasks
+    //var tasks = [];
     //put user input into a var
     var inputValue = inputTask.value;
     //create the var to contain the user input
     theLi = document.createElement('li');
     //change the inner html in javascript to display items in a list with buttons and checkbox
     newLi = theLi.innerHTML = '<input type="checkbox">' + inputValue +
-    '<button id="edit" type="submit" class="btn btn-default">Edit</button>' +
-    '<button id="del" type="submit" class="btn btn-default">Delete</button>';
+    '<button type="submit" class="btn btn-default edit">Edit</button>' +
+    '<button type="submit" class="btn btn-default del">Delete</button>';
 
     //add item to the list
     list.appendChild(theLi);
 
     //push li to tasks array
-    tasks.push(inputValue);
+    //tasks.push(inputValue);
     //add item to the list on
     //list.appendChild(tasks);
 
-    console.log(tasks);
+    console.log(inputValue);
     //clear the input field
     inputTask.value = '';
+    setUp();
+}
+
+function setUp(){
+    //grab the delete/edit buttons from the HTML
+    var delBtns = document.querySelectorAll('.del');
+    //add event listener to button to run a function when clicked
+    debugger
+    for ( var i = 0; i < delBtns.length; i++) {
+
+        //what does this do... make a var out of the delBtn? yea..
+        var delBtn = delBtns[i];
+
+        //this attaches an event listener to the click event for the delBtn and runs the removeItem func when clicked
+        delBtn.addEventListener('click', removeItem);
+
+        //function removes the list item it's within and when clicked confirm they really want to delete
+
+    }
+
+}
+
+function removeItem(){
+    alert('hi there');
+    this.parentNode.outerHTML = '';
 }
 
 //grab the edit button from the HTML
-var edit = document.querySelectorAll('#edit');
+var edit = document.querySelectorAll('.edit');
 //add event listener to button to run function when clicked
 for (i = 0; i < edit; i++) {
     var editBtn = edit[i];
@@ -56,14 +81,3 @@ function editItem(){
 
 }
 
-//grab the delete button from the HTML
-var del = document.querySelectorAll('#del');
-//add event listener to button to run a function when clicked
-for (i = 0; i < del; i++) {
-    var delBtn = del[i];
-    delBtn.addEventListener('click', removeItem);
-}
-//function removes the list item it's within and when clicked confirm they really want to delete
-function removeItem(){
-    theLi.remove();
-}
