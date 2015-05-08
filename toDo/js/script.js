1a2731d255d75d850d7a56b010b7b62895708c89
purplyrocky <purplyrocky@gmail.com>
2015-05-07 19:29:26 -0700

    removing the appending for tasks var

--- toDo/js/script.js
+++ toDo/js/script.js
@@ -5,6 +5,11 @@ var myBtn = document.getElementById('myBtn');
 //add event listener to button to run a function when clicked
 myBtn.addEventListener('click', createTask);
 
+//declaring global vars
+var theLi;
+
+var newLi;
+
 //grab the ul from the HTML
 var list = document.getElementById('list');
 
@@ -20,9 +25,9 @@ function createTask(e){
     //put user input into a var
     var inputValue = inputTask.value;
     //create the var to contain the user input
-    var theLi = document.createElement('li');
+    theLi = document.createElement('li');
     //change the inner html in javascript to display items in a list with buttons and checkbox
-    var newLi = theLi.innerHTML = '<input type="checkbox">' + inputValue +
+    newLi = theLi.innerHTML = '<input type="checkbox">' + inputValue +
     '<button id="edit" type="submit" class="btn btn-default">Edit</button>' +
     '<button id="del" type="submit" class="btn btn-default">Delete</button>';
 
@@ -32,7 +37,7 @@ function createTask(e){
     //push li to tasks array
     tasks.push(inputValue);
     //add item to the list on
-    list.appendChild(tasks);
+    //list.appendChild(tasks);
 
     console.log(tasks);
     //clear the input field
@@ -40,16 +45,25 @@ function createTask(e){
 }
 
 //grab the edit button from the HTML
-var edit = document.getElementById('edit');
+var edit = document.querySelectorAll('#edit');
 //add event listener to button to run function when clicked
-edit.addEventListener('click', editItem() {
-    //function should turn item into input field then change back to item when "enter" key is pressed
+for (i = 0; i < edit; i++) {
+    var editBtn = edit[i];
+    editBtn.addEventListener('click', editItem);
+}
+//function should turn item into input field then change back to item when "enter" key is pressed
+function editItem(){
 
-//});
+}
 
 //grab the delete button from the HTML
-//var del = document.getElementById('del');
+var del = document.querySelectorAll('#del');
 //add event listener to button to run a function when clicked
-//del.addEventListener('click', removeItem() {
-    //function removes the list item it's within, when clicked confirm they really want to delete
-//});
+for (i = 0; i < del; i++) {
+    var delBtn = del[i];
+    delBtn.addEventListener('click', removeItem);
+}
+//function removes the list item it's within and when clicked confirm they really want to delete
+function removeItem(){
+    theLi.remove();
+}
